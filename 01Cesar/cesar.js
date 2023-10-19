bece48cd01a8d219c488ca73ab75ad25a29a1b1f
Max1SR <msanchezr2107@alumno.ipn.mx>
2023-10-18 21:56:07 -0600

    Cesar solito

---
+++ 01Cesar/cesar.js
@@ -0,0 +1,32 @@
+window.onload = function() {
+
+const desplazamiento = document.getElementById("desplazamiento");
+const texto = document.getElementById("texto");
+const textoCifrado = document.getElementById("cifrado");
+
+//Vamos a crear una funciona para poder cifrar
+// investigar que el un valor ternario
+function cifrado(){
+    //declarar el texto a cifrar
+    const textoIngresado = texto.value;
+    textoCifrado.value = textoIngresado.split('').map(c=>{
+        let mayus = (c === c.toUpperCase()) ? true : false; 
+        let valorEntero = c.toLowerCase().charCodeAt(0);
+        if(valorEntero>= 97 && valorEntero<=122){
+            const valorDesplazamiento = parseInt(desplazamiento.value);
+            if(valorEntero + valorDesplazamiento > 122){
+                valorEntero = 97 + (valorEntero - 122) + valorDesplazamiento - 1;
+            }else{
+                valorEntero = valorEntero + valorDesplazamiento
+            }
+        }
+        let cifrado = String.fromCharCode(valorEntero);
+        return mayus ? cifrado.toUpperCase() : cifrado;
+    }).join('');
+};
+
+texto.addEventListener('keyup', cifrado);
+
+
+desplazamiento.addEventListener("change", cifrado);
+}
